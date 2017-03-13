a6fb38c710306868eae16cc944f91a3a5ba16b61
Gyöngyösi András <gyongy96@gmail.com>
2017-03-13 07:38:03 -0700

    first commit

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -1,12 +1,26 @@
 package hu.bme.mit.train.controller;
 
 import hu.bme.mit.train.interfaces.TrainController;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class TrainControllerImpl implements TrainController {
 
 	private int step = 0;
 	private int referenceSpeed = 0;
 	private int speedLimit = 0;
+
+
+	public void runTimer(){
+		Timer timer = new Timer();
+		timer.schedule(new TimerTask() {
+
+			@Override
+			public void run() {
+				followSpeed();
+			}
+		}, 0, 200);
+	}
 
 	@Override
 	public void followSpeed() {
